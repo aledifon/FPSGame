361a259a808ad77c1d3c7b858b727bafd70fa649
aledifon <aledifon89@gmail.com>
2025-01-22 14:40:08 +0100

    Imported Unity Package with weapons & Particle System to the Project + Created Shooting script

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] float weaaponDamage;
+    [SerializeField] float weaponRange;             // The weapon range
+    [SerializeField] float weaponImpactForce;       // Impact bullet force on the target
+    [SerializeField] float weaponCadence;           // Weapon Cadence
+    [SerializeField] ParticleSystem muzzleEffect;   // Weapon Particle system
+
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
