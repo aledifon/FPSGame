57fd4734f508842efbcbdba307e5bf7d91eb29ce
aledifon <aledifon89@gmail.com>
2025-01-30 03:31:42 +0100

    Some modifs related with sounds FX and Walking Simulator Scene

--- Assets/Scripts/Events/TriggerDoorEvent.cs
+++ Assets/Scripts/Events/TriggerDoorEvent.cs
@@ -10,7 +10,9 @@ public class TriggerDoorEvent : MonoBehaviour
 
     [SerializeField] Transform doorTransform; //door (6)
     [SerializeField] float openingDuration;
-    Quaternion targetDoorRot = Quaternion.Euler(0f, -360f, 0f);
+    //Quaternion targetDoorRot = Quaternion.Euler(0f, -360f, 0f);    
+    //Quaternion rotationToAdd /*= Quaternion.Euler(0f, 90f, 0f)*/;
+    Quaternion targetDoorRot;
     Quaternion startDoorRot;
 
     // Start is called before the first frame update
@@ -18,21 +20,21 @@ public class TriggerDoorEvent : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();         
         
-        doorTransform = GetComponent<Transform>();        
+        doorTransform = GetComponent<Transform>();
 
         //doorTransform.rotation = Quaternion.Euler(0f, -450f, 0f);
 
-        if (doorTransform != null)
-        {
-            // Gira la puerta al ángulo deseado
-            doorTransform.localRotation = Quaternion.Euler(0f, -450f, 0f); // Gira a -90 grados en Y
-            Debug.Log("Door rotated to: " + doorTransform.localRotation.eulerAngles); // Muestra la rotación en la consola
-        }
-        else
-        {
-            Debug.LogError("doorTransform no está asignado.");
-        }
-        startDoorRot = doorTransform.localRotation;        
+        //if (doorTransform != null)
+        //{
+        //    // Gira la puerta al ángulo deseado
+        //    doorTransform.rotation = Quaternion.Euler(0f, -450f, 0f); // Gira a -90 grados en Y
+        //    Debug.Log("Door rotated to: " + doorTransform.rotation.eulerAngles); // Muestra la rotación en la consola
+        //}
+        //else
+        //{
+        //    Debug.LogError("doorTransform no está asignado.");
+        //}
+        //startDoorRot = doorTransform.rotation;        
     }
 
     // Update is called once per frame
@@ -46,7 +48,7 @@ public class TriggerDoorEvent : MonoBehaviour
         if (other.CompareTag("Player") && !audioSource.isPlaying)
         {
             audioSource.Play();
-            StartCoroutine(OpenDoor());
+            //StartCoroutine(OpenDoor());
             StartCoroutine(DestroyAfterDelay());
         }                
     }
@@ -59,6 +61,10 @@ public class TriggerDoorEvent : MonoBehaviour
     {
         float elapsedTime = 0f;
         startDoorRot = doorTransform.rotation;
+        targetDoorRot = Quaternion.Euler(0f, 360f, 0f);
+
+        Debug.Log("Door started on: " + startDoorRot.eulerAngles); // Muestra la rotación en la consola
+        Debug.Log("Target rotation is: " + targetDoorRot.eulerAngles); // Muestra la rotación en la consola
 
         while (elapsedTime < openingDuration) 
         {
@@ -66,7 +72,7 @@ public class TriggerDoorEvent : MonoBehaviour
             elapsedTime += Time.deltaTime;
 
             // Move door from point A towards point B  
-            transform.rotation = 
+            doorTransform.rotation = 
                 Quaternion.Lerp(startDoorRot, targetDoorRot, elapsedTime / openingDuration);
             // 1 frame Waiting
             yield return null;
@@ -75,5 +81,7 @@ public class TriggerDoorEvent : MonoBehaviour
         doorTransform.rotation = targetDoorRot;
         // Set the Door opened flag to true
         isDoorOpened = true;
+
+        Debug.Log("Door rotated to: " + doorTransform.rotation.eulerAngles); // Muestra la rotación en la consola
     }
 }
